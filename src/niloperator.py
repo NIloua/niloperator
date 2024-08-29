3b0c307f70fe5c1104fbb2f2e6f6670f3eb0953b
NIloua <53289964+NIloua@users.noreply.github.com>
2024-08-29 17:14:36 +0200

    Add new methods

--- src/niloperator.py
+++ src/niloperator.py
@@ -1,3 +1,33 @@
 class Calculator:
     def __init__(self):
         self.history = []
+
+    def add(self, a, b):
+        ans = a + b
+        self.history.append(f"{a} + {b} = {ans}")
+        return ans
+
+    def subtract(self, a, b):
+        ans = a - b
+        self.history.append(f"{a} - {b} = {ans}")
+        return ans
+
+    def multiply(self, a, b):
+        ans = a * b
+        self.history.append(f"{a} * {b} = {ans}")
+        return ans
+
+    def division(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError()
+        ans = a / b
+        self.history.append(f"{a} / {b} = {ans}")
+
+        return ans
+
+    def view_history(self):
+        return self.history
+
+    def clear_history(self):
+        self.history = []
+        return "Cleared successfully!"
