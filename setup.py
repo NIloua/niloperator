0783035f1d7a72d6ce1e38e406cad7a5f48bc707
NIloua <53289964+NIloua@users.noreply.github.com>
2024-08-29 16:23:54 +0200

    Add

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="niloperataor",
+    version="0.0.0",
+    author="NIloua",
+    author_email="niloufarhykh@yahoo.com",
+    url="https://github.com/NIloua/niloperator?tab=readme-ov-file",
+    description="This repository is dedicated to a software that calculates arithmetic operations involving Nilou",
+    package_dir={"": "src"},
+    py_modules="niloperataor",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+)
